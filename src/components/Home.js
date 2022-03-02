95a2dfe271ded1deb6dc81b864c3ebd750df8d24
kirubhavignesh <imkiru7@gmail.com>
2022-03-02 12:05:12 +0530

    Updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,7 +29,7 @@ const Home = () => {
         setLoading(true)
         if(validateUrl()){
             try{
-                const response = await fetch(shorternLink)
+                const response = await fetch(shortenLink)
                 const result = await response.json()
                 setResultUrl(result)
             }catch{
@@ -40,9 +40,9 @@ const Home = () => {
     }
 
     return (
-        <div className="p-8 mt-4">
+        <div className="p-8 mt-5 sm:mt-10">
             <h1 className="text-white font-bold text-4xl sm:text-6xl">More than just<span className="bg-gradient-to-r from-indigo-200 to-indigo-400 bg-clip-text text-transparent mt-3 block font-extrabold">shorter links</span></h1>
-            <form onSubmit={handleSubmit} className="flex sm:flex-row flex-col sm:w-4/5 lg:w-3/5 mt-5">
+            <form onSubmit={handleSubmit} className="flex sm:flex-row flex-col  max-w-2xl mt-5">
                 <input onChange={(e) => setUrl(e.target.value)} type="text" placeholder="Place your link here..." className="px-4 py-2 text-gray-600 outline-none rounded w-full flex-1"/>
                 <button type="submit" className="bg-gradient-to-r sm:mt-0 text-center mt-3 sm:ml-3 from-indigo-200 to-indigo-400 text-lg text-white font-bold px-4 py-2 rounded hover:from-indigo-600 hover:to-indigo-700 transition-all flex justify-center">{loading ? <LoadingIcon/>  : 'Shorten it!'}</button>
             </form>
--- src/components/ShortURL.js
+++ src/components/ShortURL.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ShortURL = ({shortLink}) => {
   return (
-    <div className='p-8 bg-white rounded sm:w-4/5 lg:w-3/5 mt-5'>
+    <div className='p-8 bg-white rounded max-w-2xl mt-5'>
         <p className='text-green-600 font-bold text-sm'>Link created!</p>
         <a href={shortLink} target='blank' className='text-gray-600 mt-3 hover:underline'>{shortLink}</a>
     </div>
