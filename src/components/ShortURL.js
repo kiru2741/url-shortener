02bb4af2c1a16ec832a321ee1076d81a26c8dd22
kirubhavignesh <imkiru7@gmail.com>
2022-03-03 10:48:07 +0530

    Added copy to clipboard

--- src/components/ShortURL.js
+++ src/components/ShortURL.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ShortURL = ({shortLink}) => {
+
+  const [copyClicked, setCopyClicked] = useState(false)
+
+  function handleClick(){
+    setCopyClicked(true)
+    navigator.clipboard.writeText(shortLink)
+    setTimeout(()=>setCopyClicked(false), 2000)
+  }
+
   return (
     <div className='p-8 bg-white rounded max-w-2xl mt-5'>
         <p className='text-green-600 font-bold text-sm'>Link created!</p>
-        <a href={shortLink} target='blank' className='text-gray-600 mt-3 hover:underline'>{shortLink}</a>
+        <a href={shortLink} target='blank' className='text-gray-600 mt-5 mr-5 hover:underline'>{shortLink}</a>
+        <i onClick={handleClick} className={`${copyClicked ? 'fas fa-check' : 'far fa-copy'} text-xl cursor-pointer text-gray-600`}></i>
     </div>
   )
 }
